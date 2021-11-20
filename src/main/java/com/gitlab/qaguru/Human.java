d29b38764c259c8f5519edcb1561a00dd0c31238
oleksii.kravchuk <oleksii.kravchuk@pm.bet>
2021-11-20 10:14:00 +0200

    added constructor, getter, setter, static method, nested static class, fixed incorrect git ingonre

--- src/main/java/com/gitlab/qaguru/Human.java
+++ src/main/java/com/gitlab/qaguru/Human.java
@@ -2,20 +2,64 @@ package com.gitlab.qaguru;
 
 public class Human {
 
+    //непараметризованный конструктор класса
+    public Human() {
+
+        this.name = null;
+        this.age = 0;
+        this.weight = 3;
+        this.height = 40;
+    }
+
+    //параметризованный конструктор класса
+    public Human(int weight, int height, int age, String name) {
+
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+        this.name = name;
+    }
+
     String name;
     int age;
     int weight;
     int height;
+    static String commonName = "Человек";
+
+    //геттер
+    public int getWeight() {
+        return weight;
+    }
+
+    //сеттер
+    public int setWeight(int weight) {
+        return this.weight = weight;
+    }
+
+    //вложенный статический класс
+    static class PassportData {
+        String series = "GT";
+        String number = "500";
+
+        void showPassport() {
+            System.out.println("My passport is " + series + number);
+        }
+    }
 
     void sayName() {
-        System.out.println("My name is "+ name);
+        System.out.println("My name is " + name);
     }
 
     void sayAge() {
-        System.out.println("My age is "+ age);
+        System.out.println("My age is " + age);
     }
 
     void sayWeightHeight() {
         System.out.println("My weight and height are " + weight + " and " + height);
     }
+
+    static void sayCommonName() {
+        System.out.println(commonName);
+    }
+
 }
--- src/main/java/com/gitlab/qaguru/Main.java
+++ src/main/java/com/gitlab/qaguru/Main.java
@@ -3,15 +3,36 @@ package com.gitlab.qaguru;
 public class Main {
     public static void main(String[] args) {
 
+        //создание объекта с дефолтными
         Human human = new Human();
         human.name = "John";
         human.age = 24;
         human.height = 179;
-        human.weight = 84;
+        human.getWeight();
         human.sayName();
         human.sayAge();
         human.sayWeightHeight();
+        //вызов статического метода
+        Human.sayCommonName();
 
+        //создание объекта спараметрами
+        Human humanParametrised = new Human(10, 90, 5, "Petro");
+        humanParametrised.sayName();
+        humanParametrised.sayAge();
+        humanParametrised.sayWeightHeight();
+        //вызов статического метода
+        Human.sayCommonName();
+
+        //получение изменение данных геттером
+        Human humanWeight = new Human();
+        System.out.println("Вес полученный геттером " + humanWeight.getWeight());
+        System.out.println("Вес измененный сеттером " + humanWeight.setWeight(123));
+
+        //создание объекта вложенного класса и вызом его метода
+        Human.PassportData passportData = new Human.PassportData();
+        passportData.showPassport();
+
+        //взаимодействие с данными, методами объекта класса
         Car bus = new Car();
         Car truck = new Car();
         bus.fuelConsumption = 29;
